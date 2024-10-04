7f66bad6f304cf0bd6f0df2bce89a8d71fa0c153
Lamparter <notlamparter@outlook.com>
2024-10-04 17:46:36 +0100

    Add CLI

--- Genome/Program.cs
+++ Genome/Program.cs
@@ -1,48 +1,29 @@
-﻿namespace Helix.Genome
+﻿using System;
+using Helix;
+using Interface;
+
+namespace Genome
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Animals animals = new Animals();
 
-            // Example of adding instances of Animal
-            animals.AnimalList.Add(new Linnaeus
-            {
-                CommonName = "Lion",
-                UniversalName = "Panthera leo",
-                Kingdom = "Animalia",
-                Phylum = "Chordata",
-                Class = "Mammalia",
-                Order = "Carnivora",
-                Family = "Felidae",
-                Genus = "Panthera",
-                Species = "P. leo",
-                Habitat = "Savannah",
-                ConservationStatus = "Vulnerable",
-                Description = "Large carnivorous feline of Africa and India"
-            });
+            // Path to the help.json file
+            string helpFilePath = "..\\..\\..\\help.json";
 
-            animals.AnimalList.Add(new Linnaeus
-            {
-                CommonName = "Tiger",
-                UniversalName = "Panthera tigris",
-                Kingdom = "Animalia",
-                Phylum = "Chordata",
-                Class = "Mammalia",
-                Order = "Carnivora",
-                Family = "Felidae",
-                Genus = "Panthera",
-                Species = "P. tigris",
-                Habitat = "Forest",
-                ConservationStatus = "Endangered",
-                Description = "The largest cat species, most recognizable for its pattern of dark vertical stripes"
-            });
+            // Create an instance of FirstRunManager
+            var firstRunManager = new FirstRunManager("Riverside Valley", "Helix");
 
-            // Add more animals as needed
+            // Create an instance of CommandLineInterface
+            var cli = new CommandLineInterface("Helix", "Riverside", helpFilePath, firstRunManager);
 
-            Console.WriteLine("Animals added successfully!");
-            Console.ReadLine();
+            // Add commands
+            cli.AddCommand("greet", () => Console.WriteLine("Hello, user!"));
+            cli.AddCommand("exit", () => Environment.Exit(0));
+
+            // Run the CLI
+            cli.Run();
         }
     }
 }
