0db6f3dd131c832c82563b8e70d65a0763eb237f
Lamparter <notlamparter@outlook.com>
2024-10-04 07:22:29 +0100

    A good start

--- Genome/Program.cs
+++ Genome/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Helix;
+
+// See https://aka.ms/new-console-template for more information
+
+Helix.Linnaeus lion = new Linnaeus();
---
+++ Helix/Helpers/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helix.Helpers
+{
+    internal class Database
+    {
+        private List<Linnaeus> _animals = new List<Linnaeus>();
+
+        public void AddAnimal(Linnaeus animal)
+        {
+            _animals.Add(animal);
+        }
+
+        public void RemoveAnimal(Linnaeus animal)
+        {
+            _animals.Remove(animal);
+        }
+
+        public List<Linnaeus> GetAnimals()
+        {
+            return _animals;
+        }
+    }
+}
